f2eed1ba192760d0e02f9b1b1ce214659b6f0e5e
karan718 <karan.gupta92@gmail.com>
2014-01-23 01:14:18 +0000

    Part of jacobian implemented

--- runSQP.m
+++ runSQP.m
@@ -2,6 +2,8 @@ function [ V_fin ] = runSQP( V0, case_num )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+mpc = loadcase(case_num);
+
 n           = size(mpc.bus, 1);
 m           = size(mpc.branch, 1);
 genBuses    = mpc.gen(:, 1);
@@ -68,7 +70,7 @@ WMin        = mpc.bus(:, 13) .^ 2;
 
 
 
-% new code to determine sj
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% new code to determine sj
 from_verts = mpc.branch(:,1);
 to_verts = mpc.branch(:,2);
 
@@ -76,28 +78,140 @@ res = mpc.branch(:,3);
 rctn = mpc.branch(:,4);
 
 
-Adj = zeros(n);
-Adm = zeros(n)
+Adj = sparse(n,n);
+Admittance = sparse(n,n)
 for ii = 1:m
-    Adj(from_verts(ii), to_verts(ii)) = 1;
-    Adj(to_verts(ii), from_verts(ii)) = 1;
+    jj = from_verts(ii);
+    kk = to_verts(ii);
+    Adj(jj, kk) = 1;
+    Adj(kk, jj) = 1;
     
-    y_jk = complex(res(ii), -rctn(ii))/sqrt(res(ii), rctn(ii))
-    Adm(from_verts(ii), to_verts(ii)) = y_jk;
-    Adm(to_verts(ii), from_verts(ii)) = y_jk;
+    y_jk = complex(res(ii), -rctn(ii))/sqrt(res(ii)^2 + rctn(ii)^2)
+    Admittance(jj, kk) = y_jk;
+    Admittance(kk, jj) = y_jk;
 end
 
 % to ensure that diagonal entries are zero
 Adj(logical(eye(n)))=0;
 
+rAdmittance = real(Admittance);% real part of admittance matrix
+iAdmittance = imag(Admittance);% imaginary part of admittance matrix
 
+nbrs = cell(1,n);
+for jj = 1:n
+    nbrs{jj} = find(Adj(jj,:));
+end
+
+
+% Following is code for iterative step which takes as input V_k
 % iterative step to determine s for each iteration
-% assuming V_k has been determined
+% assuming V_k (column vector) has been determined
 
-% matrix containing jth row multiplied by (V_k)_j
-A = Adj * diag(V_k);
-B = ((temp-temp').*Adm) * diag(V_k);
-s = B(logical(eye(n)));
+
+
+% % matrix containing jth row multiplied by (V_k)_j
+% A = Adj * diag(V_k);
+% B = ((temp-temp').*Admittance) * diag(V_k);
+% s = B(logical(eye(n))); % this a part of g(x_k) in the book chapter 4
+% 
+% num_ineq = 2*(n+m); % min max for voltages and branch powers
+% 
+% lambda = zeros(num_ineq, 1);
+
+
+
+
+% To determine the cost function which is dependent only on the real part
+% as is seen in the solveOPF cvx, since cost is function of Pg only
+
+s = zeros(n,1);
+
+for jj = 1:n
+    s(jj) = abs(V_k(jj))^2*sum(Admittance(:,jj)) - V_k(jj)*sum(Admittance(jj,:)*V_k);
+end
+Pinj = real(s);
+Qinj = imag(s);
+
+Vsq = abs(V_k).^2;
+rV_k = real(V_k);% real part of voltage vector
+iV_k = imag(V_k);% imaginary part of voltage vector
+% no we have the constant parts of the inequalities . Need to evaluate the linear parts
+% for that we use jacobian matrix in the various components
+
+% To determine the jacobian matrices
+jacobian_Pinj = sparse(2*n, n);
+jacobian_Qinj = sparse(2*n, n);
+for jj = 1:n
+    
+    jacobian_Pinj(2*jj-1,jj) = 2*rV_k(jj)*sum(rAdmittance(jj,:)) ...
+        + rAdmittance(jj,:)*rV_k ...
+        - iV_k(jj)*sum(iAdmittance(jj,:)) ...
+        + iAdmittance(jj,:)*iV_k ...
+        + iV_k(jj)*rAdmittance(JJ,:)*iV_k; % d /d real()
+    jacobian_Pinj(2*jj, jj) = -2*iV_k(jj)*rAdmittance(jj,:)*iV_k ...
+        + rV_k(jj)*iAdmittance(jj,:)*rV_k ...
+        - rV_k(jj)*iAdmittance(jj:1)*i
+    jacobian_Qinj
+    jacobian_Qinj
+    
+    nbrs_jj = nbrs{jj}
+    for ii = 1:size(nbrs_jj, 2)
+        jacobian_Pinj(2*jj-1, ii) = 
+        jacobian_Pinj(2*jj, ii) = 
+        jacobian_Qinj
+        jacobian_Qinj
+    end
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%55
+% this is to be done multipe times
+cvx_begin
+    variables Pg(n) Qg(n) Pinj(n) Qinj(n) Vsq(n) aux(n); 
+    variables Pf(m) Pt(m);
+    variable W(n, n) hermitian
+    minimize sum(aux)
+    subject to
+        
+        
+        for kk = 1:n
+            Pinj(kk) == real( trace( Phi{kk} * W ));
+            Qinj(kk) == real( trace( Psi{kk} * W ));
+            Vsq(kk)  == W(kk, kk);
+            
+            costGen2(kk) * Pg(kk)^2 ...
+                + costGen1(kk) * Pg(kk) ...
+                + costGen0(kk) <= aux(kk);
+        end
+    
+        Pinj == Pg - Pd;
+        Qinj == Qg - Qd;
+    
+        
+        Pg <= PgMax;
+        Pg >= PgMin;
+        Qg <= QgMax;
+        Qg >= QgMin;
+        Vsq >= WMin;
+        Vsq <= WMax;
+        
+%         % Line limits
+%         for bb = 1:m
+%             Pf(bb) == real(trace(Ff{bb} * W));
+%             Pt(bb) == real(trace(Tt{bb} * W));
+%         end
+%              
+%         Pf <= Fmax;
+%         Pt <= Fmax;
+
+        for i = 1:length(mpc.branch(:, 1))
+            fr = mpc.branch(i:i, 1);
+            to = mpc.branch(i:i, 2);
+            temp_matrix = [[W(fr, fr), W(fr, to)]', [W(to, fr), W(to, to)]'];
+            temp_matrix == hermitian_semidefinite( 2 );
+        end
+         
+cvx_end
 
 end
 
