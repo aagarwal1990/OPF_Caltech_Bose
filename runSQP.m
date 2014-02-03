c2b865185b2265e5cb108b6ffa2e14dcc0b7c865
Anish Agarwal <aagarwal@caltech.edu>
2014-02-02 16:12:06 -0800

    changed sign of Tt(kk) constraint

--- runSQP.m
+++ runSQP.m
@@ -233,7 +233,7 @@ while and(iter_diff > 10^-4, count < 10)
     for kk = 1:m
          hess_lagrangian = hess_lagrangian ...
                           + 2*lambda_k(6*n+kk, :)*exp_Ff{kk} ...
-                          + 2*lambda_k(6*n+m+kk, :)*exp_Tt{kk};
+                          - 2*lambda_k(6*n+m+kk, :)*exp_Tt{kk};
     end
    
     cvx_begin
@@ -244,8 +244,8 @@ while and(iter_diff > 10^-4, count < 10)
         minimise obj;
         subject to
             obj == grad_lagrangian * (exp_V-exp_V_k); ...
-%                         + 1/2 * (exp_V-exp_V_k)'* hess_lagrangian ...
-%                         * (exp_V - exp_V_k);
+                        + 1/2 * (exp_V-exp_V_k)'* hess_lagrangian ...
+                        * (exp_V - exp_V_k);
             for kk = 1:n
                 Pinj(kk) == exp_V_k'* exp_Phi{kk} * exp_V_k ;
                 Qinj(kk) == exp_V_k'* exp_Psi{kk} * exp_V_k;
